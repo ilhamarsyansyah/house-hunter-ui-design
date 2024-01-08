41bbb52734cfc985498aafe741eaa4269f07e7b1
ilhamarsyansyah <arsyansyah86@gmail.com>
2024-01-08 17:58:32 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        PrimaryColor : '#1B1C57',
+        SecondaryColor : '#626687',
+        Yellow : '#F59E0B',
+        Green : '#10B981',
+        Abu : '#888B97',
+
+
+      },
+      fontFamily: {
+        Lexend: ['Lexend wght']
+      }
+    },
+  },
+  plugins: [],
+}
